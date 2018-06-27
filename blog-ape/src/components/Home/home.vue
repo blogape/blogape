690a2eb842c48994ff83be6196261a9b83f66075
zengts <905080195@qq.com>
2018-06-27 11:49:06 +0800

    上传项目源代码

---
+++ blog-ape/src/components/Home/home.vue
@@ -0,0 +1,19 @@
+<template>
+<!-- 首页 -->
+<div class="home">
+<TopNav></TopNav>
+</div>
+</template>
+
+<script>
+import TopNav from '../topNav/topNav.vue';
+export default {
+comments:{
+  TopNav,
+}
+}
+</script>
+
+<style>
+
+</style>
